b64e353de39e42ac53b0903bf709e0d63ceee7cd
mwas <francismwangi152@gmail.com>
2018-12-11 18:03:18 +0300

    adding more buckets.

--- exporter.go
+++ exporter.go
@@ -116,7 +116,9 @@ func (c *HistogramContainer) Get(metricName string, labels prometheus.Labels, he
 			Name:        metricName,
 			Help:        help,
 			ConstLabels: labels,
-			Buckets:     []float64{1, 2, 5, 10, 20, 60},
+			Buckets: []float64{0.125, 0.25, 0.5, 1, 2, 5, 7, 10, 15, 20, 25, 30, 40, 50, 60, 70,
+				80, 90, 100, 200, 300, 400, 500, 1000,
+				2000, 5000, 10000, 30000, 60000},
 		})
 		if err := prometheus.Register(histogram); err != nil {
 			return nil, err
